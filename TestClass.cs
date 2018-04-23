bd88ae12897e2ec3152fc06a497b590bb98a598b
Ramon Agramon <ramonagramon2@gmail.com>
2018-04-23 11:31:37 -0600

    Revicion2

--- TestClass.cs
+++ TestClass.cs
@@ -10,7 +10,42 @@ namespace Examen
     [TestFixture]
     public class TestClass
     {
-   
+        [Test]
+        public void Suma()
+        {
+            int[] Numeros = { 5, 7, 8, 8, 9, 9, 9, 9, 10, 10 };
+            int Resultado = 84;
+            Arimetrica x = new Arimetrica();
+            var Suma = x.Suma(Numeros);
+            Assert.AreEqual(Resultado, Suma);
+        }
+        [Test]
+        public void Promedio()
+        {
+            double[] Numeros = { 5, 7, 8, 8, 9, 9, 9, 9, 10, 10 };
+            double esperado = 8.4;
+            Arimetrica x = new Arimetrica();
+            var Promedio = x.Promedio(Numeros);
+            Assert.AreEqual(esperado, Promedio);
+        }
+        [Test]
+        public void Moda()
+        {
+            int[] Numeros = { 5, 7, 8, 8, 9, 9, 9, 9, 10, 10 };
+            int Resultado = 9;
+            Arimetrica x = new Arimetrica();
+            var Promedio = x.Moda(Numeros);
+            Assert.AreEqual(Resultado, Promedio);
+        }
+        [Test]
+        public void Medaina()
+        {
+            int[] Numeros = { 5, 7, 8, 8, 9, 9, 9, 9, 10, 10 };
+            int Resultado = 9;
+            Arimetrica x = new Arimetrica();
+            var Promedio = x.Mediana(Numeros);
+            Assert.AreEqual(Resultado, Promedio);
+        }
     }
 }
 
